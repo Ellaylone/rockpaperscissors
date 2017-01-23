42639027a462721b58ef29bac12ba5d5b866da8e
Ellaylone <ellaylone@yandex-team.ru>
2017-01-23 23:03:53 +0300

    remove module export

--- bin/rock-paper-scissors.js
+++ bin/rock-paper-scissors.js
@@ -1,54 +1,52 @@
 #!/usr/bin/env node
 
 'use strict';
-module.exports = function() {
-    const readline = require('readline'),
-          choises = [
-              "scissors",
-              "paper",
-              "rock"
-          ];
+const readline = require('readline'),
+      choises = [
+          "scissors",
+          "paper",
+          "rock"
+      ];
 
-    let userChoice,
-        userInput,
-        pcChoice;
+let userChoice,
+    userInput,
+    pcChoice;
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-    function playGame () {
-        rl.question('Your choice beatch[rock|paper|scissors]?', (answer) => {
-            pcChoice = Math.floor(3 * Math.random());
-            userInput = answer.toLowerCase();
-            userChoice = choises.indexOf(userInput);
+function playGame () {
+    rl.question('Your choice beatch[rock|paper|scissors]?', (answer) => {
+        pcChoice = Math.floor(3 * Math.random());
+        userInput = answer.toLowerCase();
+        userChoice = choises.indexOf(userInput);
 
-            if (userChoice < 0) {
-                console.log('I don\'t understand.');
-            } else {
-                switch (userChoice) {
-                    case pcChoice:
-                        console.log('draw');
-                        break;
-                    case pcChoice + 1:
-                    case pcChoice - 2:
-                        console.log('lose');
-                        break;
-                    case pcChoice - 1:
-                    case pcChoice + 2:
-                        console.log('win');
-                        break;
-                    default:
-                        console.log('alarm! something went wrong');
-                        break;
-                }
-
-                console.log(choises[userChoice], choises[pcChoice]);
+        if (userChoice < 0) {
+            console.log('I don\'t understand.');
+        } else {
+            switch (userChoice) {
+                case pcChoice:
+                    console.log('draw');
+                    break;
+                case pcChoice + 1:
+                case pcChoice - 2:
+                    console.log('lose');
+                    break;
+                case pcChoice - 1:
+                case pcChoice + 2:
+                    console.log('win');
+                    break;
+                default:
+                    console.log('alarm! something went wrong');
+                    break;
             }
-            playGame();
-        });
-    }
 
-    playGame();
+            console.log(choises[userChoice], choises[pcChoice]);
+        }
+        playGame();
+    });
 }
+
+playGame();
